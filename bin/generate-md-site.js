fa19a4dd51071166f501f4aa87f02c4b35d5a8c4
Fisher <fisher.subs@gmail.com>
2018-07-23 16:52:43 +0800

    [bin/generate-md-site.js] Added command line script named 'generate-md-site'.

--- bin/generate-md-site.js
+++ bin/generate-md-site.js
@@ -1,7 +1,7 @@
-'use strict';
+#!/usr/bin/env node
 
 const path = require('path');
-const App = require('./src/app');
+const App = require('../src/app');
 const configsModule = '.site_configs';
 
 const targetDir = process.argv[2];
