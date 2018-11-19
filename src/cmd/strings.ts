58158fbeedd98ec9363a235c92f78e595c2bc35a
Yu Zhanbei <awesome.zhanbei@gmail.com>
2018-11-19 16:34:06 +0800

    [src/cmd/strings] Highlighted user-input content; updated logging styles.

--- src/cmd/strings.ts
+++ src/cmd/strings.ts
@@ -6,19 +6,22 @@ import * as logger from './logger';
 
 const MODULE_SITE_CONFIGS = constants.MODULE_SITE_CONFIGS;
 
+// Emphasize the user-input file and folder.
+const emTarget = (dir) => `"${dir}"`;
+
 // Error an specified error.
 // helpError for an specified error.
 // Help, notice, hint, .
 export const getGenerateMdSiteErrors = (targetSiteDir, targetSiteDirLocation, targetConfigsDir, resolvedTargetConfigsDirLocation) => ({
-	noSiteConfigsModulesFoundError: `No \`${MODULE_SITE_CONFIGS}\` module found in the target dir or the target dir ${targetSiteDir} is not a node module.`,
+	noSiteConfigsModulesFoundError: `No \`${emTarget(MODULE_SITE_CONFIGS)}\` module found in the target dir or the target dir ${emTarget(targetSiteDir)} is not a node module.`,
 	noSiteConfigsModulesFoundHelp: `You may add a config file named \`index.js\`, \`index.json\`, \`${MODULE_SITE_CONFIGS}.js\`, \`${MODULE_SITE_CONFIGS}.json\`, \`${MODULE_SITE_CONFIGS}/index.js\`, or \`${MODULE_SITE_CONFIGS}/index.json\` to the target dir.`,
 
-	targetSiteDirIsNotFolderError: `The target folder resolved "${targetSiteDirLocation}" is not a folder.`,
+	targetSiteDirIsNotFolderError: `The target folder resolved ${emTarget(targetSiteDirLocation)} is not a folder.`,
 
-	targetSiteConfigsDirExistedError: `Found existed folder of the site configs "${targetConfigsDir}" from the given target dir.`,
-	targetSiteConfigsDirExistedHelp: `Remove the folder "${targetConfigsDir}" first before initializing site.`,
+	targetSiteConfigsDirExistedError: `Found existed folder of the site configs ${targetConfigsDir} from the given target dir.`,
+	targetSiteConfigsDirExistedHelp: `Remove the folder ${emTarget(targetConfigsDir)} first before initializing site.`,
 
-	nonEmptySiteConfigsResolvedError: `Non-empty configures resolved "${resolvedTargetConfigsDirLocation}" from "${targetSiteDir}".`,
+	nonEmptySiteConfigsResolvedError: `Non-empty configures resolved ${emTarget(resolvedTargetConfigsDirLocation)} from ${emTarget(targetSiteDir)}.`,
 	nonEmptySiteConfigsResolvedHelp: `Cannot initialize site with valid configures; expected empty configures.`,
 
 	invalidSiteConfigsResolvedError: 'Invalid site configs resolved:',
@@ -26,30 +29,35 @@ export const getGenerateMdSiteErrors = (targetSiteDir, targetSiteDirLocation, ta
 });
 
 export const getGenerateMdSiteNotices = (targetSiteDir, targetSiteDirLocation, targetConfigsDir, resolvedTargetConfigsDirLocation) => ({
-	noticeSiteConfigsResolvedOkay: `The configures resolved "${resolvedTargetConfigsDirLocation}" from "${targetSiteDir}" is ok.`,
+	noticeSiteConfigsResolvedOkay: `The configures resolved ${emTarget(resolvedTargetConfigsDirLocation)} from ${emTarget(targetSiteDir)} is ok.`,
 });
+
+// Highlight messages as info.
+const info = (msg) => logger.FG_INFO + msg + logger.FG_RESET;
+// Highlight the targeted user-input file and dir.
+const hlTarget = (dir) => `"${logger.FG_INFO}${dir}${logger.FG_RESET}"`;
 
 // const givenTargetConfigsIndexJs = ;
 export const getSiteInitializationNotices = (targetSiteDir, targetSiteDirLocation, targetConfigsDir) => ({
-	noticeTaskInitializingExistedMdSite: `You are ${logger.FG_INFO}initialize an *existed* markdown site${logger.FG_RESET} "${targetSiteDirLocation}" with prompts and the default templates.`,
-	noticeTaskCreatingNewMdDemoSite: `You are ${logger.FG_INFO}creating a *new* markdown demo site${logger.FG_RESET} to "${targetSiteDirLocation}" with prompts and the default templates.`,
+	noticeTaskInitializingExistedMdSite: `You are ${info('initialize an *existed* markdown site')} ${hlTarget(targetSiteDirLocation)} with prompts and the default templates.`,
+	noticeTaskCreatingNewMdDemoSite: `You are ${info('creating a *new* markdown demo site')} to ${hlTarget(targetSiteDirLocation)} with prompts and the default templates.`,
 
 	noticeSiteInitializingWithPrompt: 'Now we will assist you with the process.',
 
-	noticeAbortingCauseInvalidOptions: 'Aborting, cause received invalid options...',
+	noticeAbortingCauseInvalidOptions: logger.FG_ERROR + 'Aborting, cause received invalid options...' + logger.FG_RESET,
 	noticeAwesomeCauseValidOptions: 'Awesome!',
 
 	// The same as initializing existed md site.
-	noticeWillGenerateSiteConfigsToExistedSite: `Will generate configs to the existed site: "${targetSiteDirLocation}" with the default templates.`,
-	noticeWillCreateNewMdDemoSite: `Will create a *new* markdown demo site to "${targetSiteDirLocation}" with the default templates.`,
+	noticeWillGenerateSiteConfigsToExistedSite: `Will generate configs to the existed site: ${hlTarget(targetSiteDirLocation)} with the default templates.`,
+	noticeWillCreateNewMdDemoSite: `Will create a *new* markdown demo site to ${hlTarget(targetSiteDirLocation)} with the default templates.`,
 
-	noticeCopiedMdDemoSite: `Copied a markdown demo site to "${targetSiteDir}" for you.`,
+	noticeCopiedMdDemoSite: `Copied a markdown demo site to ${hlTarget(targetSiteDir)} for you.`,
 
-	noticeCreatedSiteConfigsDir: `Created folder for site configures: "${targetConfigsDir}".`,
+	noticeCreatedSiteConfigsDir: `Created folder for site configures: ${hlTarget(targetConfigsDir)}.`,
 
-	noticeGeneratedSiteConfigsEntrance: `Generated configures file: "${path.join(targetConfigsDir, constants.INDEX_DOT_JS)}".`,
+	noticeGeneratedSiteConfigsEntrance: `Generated configures file: ${hlTarget(path.join(targetConfigsDir, constants.INDEX_DOT_JS))}.`,
 
-	noticeAddSiteConfigsToVcs: `You may add the site configures(${targetConfigsDir}) to Git(or other VCS) to track changes.`,
+	noticeAddSiteConfigsToVcs: `You may add the site configures(${hlTarget(targetConfigsDir)}) to Git(or other VCS) to track changes.`,
 
 	noticeDoneSiteInitializing: `Done!`,
 });
